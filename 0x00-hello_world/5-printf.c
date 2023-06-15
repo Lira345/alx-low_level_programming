fc3695c42b4977324c1d136d37821372984128e0
Lerato Kgodumo <kgodumo345@gmail.com>
2023-06-15 05:35:23 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 /**
  * Description: main - prints out a string to stdout.
  * Return: Always 0 (Success)
- */  
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
