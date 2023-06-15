d8a83a9ded0a68140a2a48f9f1057b001a1d8ce7
Lerato Kgodumo <kgodumo345@gmail.com>
2023-06-15 00:16:45 -0700

    0-preprocessor

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,2 @@
-#include <stdio.h>
 
-/**
-*main – Entry point
-*Return: Always 0 (success)
-*/
-
-Int main(void)
-{
-     puts(“/”programming is like building a multilingual puzzle”);
- return (0);
-}
 
