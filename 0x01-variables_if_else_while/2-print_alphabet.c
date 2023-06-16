96002e76613b534daa3732f0733c40dbac28f8f8
Lerato Kgodumo <kgodumo345@gmail.com>
2023-06-16 07:16:35 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <type.h>
+
+/**
+ * main - Prints the alphabets
+ * Return: Always 0
+ */
+int main(void)
+{
+int lowercase = 'a';
+while (lowecase < = 'z')
+putchar (lowercase);
+lowercase + = 1;
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * mai - Print all letters except q and e
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int lowercase = 'a';
+while (lowercase < = 'z')
+if (lowercase = = 'e' || lowercase = = 'q')
+lowercase + = 1;
+else
+putchar(lowercase);
+lowercase + = 1;
+putchar('\n');
+return (0);
+}
