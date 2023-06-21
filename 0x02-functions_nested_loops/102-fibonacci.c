e744ac3e6892ab62eb18f5ef0fb1ecb49a23594e
Lerato Kgodumo <kgodumo345@gmail.com>
2023-06-20 23:17:15 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+* main - prints the first 52 fibonacci numbers
+* Return: Nothing!
+*/
+
+int main(void)
+{
+int m = 0;
+long n = 1, s = 2;
+
+while (m < 50)
+{
+if (m == 0)
+printf("%ld", n);
+else if (m == 1)
+printf(", %ld", s);
+else
+{
+s += n;
+n = s - n;
+printf(", %ld", s);
+}
+m++
+}
+printf("\n");
+return (0);
+}
