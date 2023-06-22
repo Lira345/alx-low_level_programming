99444a83543cc2073e7cbf0346167585a452832f
Lerato Kgodumo <kgodumo345@gmail.com>
2023-06-22 10:38:10 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - check if a letter is upper
+ * @a: The letter to be checked
+ * Return: 1 for upper letter or 0 for any else
+ */
+
+int _isupper(int a)
+{
+	if (a >= 65 && a <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
