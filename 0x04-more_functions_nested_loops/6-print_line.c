df6670cc1657c0a6752e605ff17c30e2466c2458
Lerato Kgodumo <kgodumo345@gmail.com>
2023-06-22 11:49:28 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line according to parameter
+ * @n: The number of lines to draw
+ * Return: Empty
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < n; a++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
