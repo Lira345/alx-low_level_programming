ba9e9528bf9ae60a84ae4814c34cac1d28c37dc8
Lerato Kgodumo <kgodumo345@gmail.com>
2023-06-28 02:47:53 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,16 +9,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int a;
-
-	a = 0;
-	while (s1[a] != '\0' && s2[a] != '\0')
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		if (s1[a] != s2[a])
-		{
-			return (s1[a] = s2[a]);
-		}
-		a++;
+		s1++;
+		s2++;
 	}
-	return (0);
+	return (*s1 - *s2);
 }
