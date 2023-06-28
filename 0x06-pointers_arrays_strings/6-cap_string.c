1bb4a1529ff777acdb1a426cc04258aaf3f6134a
Lerato Kgodumo <kgodumo345@gmail.com>
2023-06-28 10:17:40 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,30 +6,30 @@
  * Return: a pointer to be changed string
  */
 
-char *cap_string(char *str)
+char *cap_string(char *n)
 {
 	int index = 0;
 
 	while (str[index])
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-		if (str[index - 1] == ' ' ||
-				str[index - 1] == '\t' ||
-				str[index - 1] == '\n' ||
-				str[index - 1] == ',' ||
-				str[index - 1] == ';' ||
-				str[index - 1] == '.' ||
-				str[index - 1] == '!' ||
-				str[index - 1] == '?' ||
-				str[index - 1] == '"' ||
-				str[index - 1] == '(' ||
-				str[index - 1] == ')' ||
-				str[index - 1] == '{' ||
-				str[index - 1] == '}' ||
-				index == 0)
-			str[index] -= 32;
+	while (!(str[index] >= 'a' && str[index] <= 'z'))
 		index++;
+	if (str[index - 1] == ' ' ||
+			str[index - 1] == '\t' ||
+			str[index - 1] == '\n' ||
+			str[index - 1] == ',' ||
+			str[index - 1] == ';' ||
+			str[index - 1] == '.' ||
+			str[index - 1] == '!' ||
+			str[index - 1] == '?' ||
+			str[index - 1] == '"' ||
+			str[index - 1] == '(' ||
+			str[index - 1] == ')' ||
+			str[index - 1] == '{' ||
+			str[index - 1] == '}' ||
+			index == 0)
+		str[index] == 32;
+	index++;
 	}
 	return (str);
 }
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,19 +9,18 @@
 char *leet(char *n)
 {
 	int m, k;
-
 	char s1[] = "aAeEoOtTlL";
 	char s2[] = "4433007711";
 
 	for (m = 0; n[m] != '\0'; m++)
-			{
-				for (k = 0; k < 10; k++)
-				{
-					if (n[m] == s1[k])
-					{
-						n[m] = s2[k];
-					}
+	{
+	for (k = 0; k < 10; k++)
+	{
+	if (n[m] == s1[k])
+	{
+	n[m] = s2[k];
+	}
 				}
-			}
-			return (n);
+	}
+	return (n);
 }
