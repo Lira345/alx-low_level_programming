b27334794bec0a6522828b79a4e8f0da99f1ab6c
Lerato Kgodumo <kgodumo345@gmail.com>
2023-07-19 06:55:14 -0700

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,19 +2,13 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name as is
- * @name: name of the person
- * @f: is pointer
-**/
-
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to the printing function
+ */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-	{
+	if (!name || !f)
 		return;
-	}
-	else
-	{
-		f(name);
-	}
+	f(name);
 }
